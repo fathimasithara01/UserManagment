8d7d9019c31b768604d1a7f56ffe7a4d1324f703
fathima sithara <fathimasiithara@gmail.com>
2025-04-15 10:27:12 +0530

    "update"

--- initalizeres/syncDb.go
+++ initalizeres/syncDb.go
@@ -1,11 +1,3 @@
-// package initalizeres
-
-// import "github.com/fathima-sithara/UserManagment/models"
-
-// func SyncDB() {
-// 	DB.AutoMigrate(&models.User{})
-// }
-
 package initalizeres
 
 import "github.com/fathima-sithara/UserManagment/models"
